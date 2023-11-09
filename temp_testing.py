e08a7addc4b47495c31c871cf6cedf56ca0e4826
Gary_MSI <garynova781@gmail.com>
2023-11-09 21:36:18 +0800

    update from MSI, check nop() amount

--- temp_testing.py
+++ temp_testing.py
@@ -10,28 +10,7 @@ led = machine.Pin(1, machine.Pin.OUT)
 def blink():
     # wrap_target()
     set(pins, 1)[0]
-    # nop()[31]
-    # nop()[31]
-    # nop()[31]
-    # nop()[31]
     set(pins, 0)[0]
-    # nop()[31]
-    # nop()[31]
-    # nop()[31]
-    # nop()[31]
-
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
-    nop()[31]
     nop()[31]
     nop()[31]
     nop()[31]
@@ -40,15 +19,39 @@ def blink():
     # wrap()
 
 
+def str_to_code(string0=""):
+    string0 = str(string0)
+    exec(string0)
+
+
 sm = rp2.StateMachine(0, blink, freq=10000000, set_base=Pin(0))
-led.value(1)
+# led.value(1)
 sm.active(1)
+sm.active(0)  # 停止PIO程序
+# led.value(0)
+
+
+# minimum delay needed for single minimum pulse
+
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
+# nop()[31]
 
 # 231109 need to test this function, but add the
 # length of pattern to enough amount
 # # 等待PIO程序完成一次执行
 # while not sm.irq().irq0:
 #     pass
-
-sm.active(0)  # 停止PIO程序
-led.value(0)
