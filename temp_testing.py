8e6a8ee16431f04d788989fb42cc054e23a8285c
Gary_surf7 <garynova781@gmail.com>
2023-11-09 09:02:19 +0800

    checking PIO function

--- temp_testing.py
+++ temp_testing.py
@@ -44,6 +44,8 @@ sm = rp2.StateMachine(0, blink, freq=10000000, set_base=Pin(0))
 led.value(1)
 sm.active(1)
 
+# 231109 need to test this function, but add the
+# length of pattern to enough amount
 # # 等待PIO程序完成一次执行
 # while not sm.irq().irq0:
 #     pass
